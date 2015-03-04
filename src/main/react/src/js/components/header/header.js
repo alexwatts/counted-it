aaa9e070d7931114cc762a6747187204eb8bf508
alexwsolidstate <alexw@solidstategroup.com>
2015-03-04 16:50:59 +0000

    [Counted it] Fixes for bootstrap JS

--- src/main/react/src/js/components/header/header.js
+++ src/main/react/src/js/components/header/header.js
@@ -19,34 +19,36 @@ var Header =
         },
         render:function() {
             return <div className="row">
-                    <nav className="navbar navbar-default">
-                        <div className="container-fluid">
-                            <div className="navbar-header">
-                                <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#counted—it-navbar-collapse-1">
-        <span className="sr-only">Toggle navigation</span>
-        <span className="icon-bar"></span>
-        <span className="icon-bar"></span>
-        <span className="icon-bar"></span>
-      </button>
-      <a className="navbar-brand" href="#">Counted It</a>
-                                </div>
-                                    <div className="collapse navbar-collapse" id="counted-it-navbar-collapse-1">
-      <ul className="nav navbar-nav navbar-right">
-        <li><a href="#"></a>{this.state.profile.profile.displayName}</li>
-        <li className="dropdown">
-          <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span className="caret"></span></a>
+                <nav className="navbar navbar-default">
+                    <div className="container-fluid">
+                        <div className="navbar-header">
+                            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                                <span className="sr-only">Toggle navigation</span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                            </button>
+                            <a className="navbar-brand" href="#">Counted It</a>
+                        </div>
+
+                        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                            <ul className="nav navbar-nav">
+                                <li className="active"><a href="#">Count Something <span className="sr-only">(current)</span></a></li>
+                                <li><a href="#">Shared Counts</a></li>
+                            </ul>
+                            <ul className="nav navbar-nav navbar-right">
+                                <li className="dropdown">
+                                    <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{this.state.profile.profile.displayName}<span className="caret"></span></a>
                                     <ul className="dropdown-menu" role="menu">
-                                        <li><a href="#">Action</a></li>
-                                        <li><a href="#">Another action</a></li>
-                                        <li><a href="#">Something else here</a></li>
+                                        <li><a href="#">Profile</a></li>
                                         <li className="divider"></li>
-                                        <li><a href="#">Separated link</a></li>
+                                        <li><a href="/logout">Logout</a></li>
                                     </ul>
                                 </li>
                             </ul>
+                            </div>
                         </div>
-                    </div>
-                </nav>
+                    </nav>
             </div>
         }
     });
