87af9ff902aa93e587ec359ea582328afb3b2ddc
alexwsolidstate <alexw@solidstategroup.com>
2015-03-05 20:01:37 +0000

    [Counted It] Added file

---
+++ src/main/react/src/js/stores/page-store.js
@@ -0,0 +1,46 @@
+/** @jsx React.DOM */
+var AppDispatcher = require('../dispatchers/app-dispatcher');
+var AppConstants = require('../constants/app-constants');
+var merge = require('react/lib/merge');
+var EventEmitter = require('events').EventEmitter;
+
+var CHANGE_EVENT = "change";
+
+//These are the count objects
+var _page = 'Home';
+
+function _updatePage(page){
+    _page = page;
+}
+
+var PageStore = merge(EventEmitter.prototype, {
+    emitChange:function(){
+        this.emit(CHANGE_EVENT)
+    },
+
+    addChangeListener:function(callback){
+        this.on(CHANGE_EVENT, callback)
+    },
+
+    removeChangeListener:function(callback){
+        this.removeListener(CHANGE_EVENT, callback)
+    },
+
+    getPage:function(){
+        return _page;
+    },
+
+    dispatcherIndex:AppDispatcher.register(function(payload){
+        var action = payload.action; // this is our action from handleViewAction
+        switch(action.actionType){
+            case AppConstants.UPDATE_PAGE:
+                _updatePage(payload.action.page);
+                break;
+        }
+        PageStore.emitChange();
+
+        return true;
+    })
+});
+
+module.exports = PageStore;
